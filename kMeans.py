46e488111457c6cba2128b9c23efa84e3ca3676c
shenhao <shenhao@xiaomi.com>
2020-03-31 23:27:38 +0800

    kmeans init

---
+++ kMeans.py
@@ -0,0 +1,22 @@
+'''
+@Time    : 2020/3/31 23:10
+@Author  : sh_lord
+@FileName: kMeans.py
+
+'''
+import csv
+import pandas as pd
+
+class kMeans:
+    def __init__(self, file_path):
+        with
+
+    def train(self):
+        pass
+
+    def predict(self):
+        pass
+
+if __name__ == '__main__':
+    file_path = "data/sku-price/skuid_price.csv"
+    kMeans = kMeans(file_path)
